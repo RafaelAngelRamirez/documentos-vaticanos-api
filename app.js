4de6cc0b28d285a28b1410e56e50269e79a2a70f
Rafael Ramirez <rafa.yael@gmail.com>
2021-03-29 10:42:21 -0600

    FIX entitytolarge

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT
 const mongoose = require("mongoose")
-const bodyParser = require("body-parser")
 const RUTAS = require("./routes/routes")
 const cors = require("cors")
 
 app.use(cors())
-app.use(bodyParser.json())
-
+app.use(express.json({limit: '10mb'}));
+app.use(express.urlencoded({limit: '10mb'}));
 mongoose
   .connect(process.env.URI, {
     useNewUrlParser: true,
